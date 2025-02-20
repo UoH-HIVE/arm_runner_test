9700c331893cd727bc27ae43d8ca55ad7ef2890b
Ash Entwisle <ash.entwisle@protonmail.com>
2025-02-20 11:22:25 +0000

    updated code to output stuff
    Signed-off-by: Ash Entwisle <ash.entwisle@protonmail.com>

--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,23 @@
 
+#include <iostream>
 #include <wiringPi.h>
 
 #define PIN_LED 13
 
 int main() {
+  std::cout << "Hello, World!" << std::endl;
+
   // switch to bcm mode
   wiringPiSetupGpio();
+  std::cout << "GPIO mode set to BCM" << std::endl;
 
   // set pin mode
   pinMode(PIN_LED, OUTPUT);
+  std::cout << "Pin mode set to OUTPUT" << std::endl;
 
   // blink LED
   while (true) {
+    std::cout << "Blinking LED" << std::endl;
     digitalWrite(PIN_LED, HIGH);
     delay(500);
     digitalWrite(PIN_LED, LOW);
